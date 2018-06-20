19c73ad0022bea043375291170090f767bccdbe3
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-06-20 11:20:23 +0700

    :lipstick: Add flip transition to card :school_satchel::cat2:

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,34 +5,16 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { Card } from './Card'
 import { StoreConsumer } from '../store'
 
-const transitionDuration = 300
+const transitionDuration = 500
 
 const TransitionContainer = styled(TransitionGroup)`
-  /* position: relative;
-  height: 100%;
-  width: 100%; */
   position: relative;
   height: 100%;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-
-  > .fade-enter {
-    opacity: 0;
-
-    &-active {
-      opacity: 1;
-    }
-  }
-
-  > .fade-exit {
-    opacity: 1;
-
-    &-active {
-      opacity: 0;
-    }
-  }
+  overflow: hidden;
 
   > div {
     position: absolute;
@@ -41,6 +23,7 @@ const TransitionContainer = styled(TransitionGroup)`
   }
 
   > div:nth-child(1) {
+    /* Backmost card */
     transform: scale(0.8) translateY(-100px);
   }
 
@@ -49,7 +32,59 @@ const TransitionContainer = styled(TransitionGroup)`
   }
 
   > div:nth-child(3) {
+    /* Frontmost card */
     transform: scale(1) translateY(0);
+  }
+
+  .eka-exit {
+    &:last-child {
+      /* Frontmost card */
+      animation: ekaExit ${transitionDuration}ms forwards;
+    }
+
+    &:first-child {
+      /* Backmost card */
+      /* opacity: 0; */
+      transform: scale(0.8) translateY(-80px);
+    }
+  }
+
+  .eka-enter {
+    &:last-child {
+      /* Frontmost card */
+      animation: ekaEnter ${transitionDuration}ms forwards;
+    }
+
+    &:first-child {
+      /* Backmost card */
+      /* opacity: 0; */
+      transform: scale(0.8) translateY(-80px);
+    }
+
+    &-active:first-child {
+      /* Backmost card */
+      /* opacity: 1; */
+      transform: scale(0.8) translateY(-100px);
+    }
+  }
+
+  @keyframes ekaExit {
+    to {
+      opacity: 0;
+      transform: translate3d(-150%, 150%, 0) rotate3d(0, 0, 1, -20deg);
+    }
+  }
+
+  @keyframes ekaEnter {
+    from {
+      opacity: 0;
+      transform: translate3d(-150%, 150%, 0) rotate3d(0, 0, 1, -20deg);
+    }
+
+    to {
+      opacity: 1;
+      transform: translate3d(0, 0, 0) rotate3d(0, 0, 0, 0);
+    }
   }
 `
 
@@ -63,7 +98,7 @@ export const Cards = () => (
           <CSSTransition
             timeout={transitionDuration}
             key={question ? question.id : index}
-            classNames="fade">
+            classNames="eka">
             {question ? <Card question={question} /> : <div />}
           </CSSTransition>
         ))
